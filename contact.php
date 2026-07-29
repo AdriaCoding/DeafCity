daf66c2b53ffb03e03810db45e5935d4a2858444
Adria <adrialisa99@gmail.com>
2026-07-29 11:07:38 +0000

    Maintenance html replaces deaf.city legacy page

--- contact.php
+++ contact.php
@@ -1,8 +1,4 @@
 <?php
 
-require("src/autoload.php");
-
-if (!empty($_POST)) $method = 'submit';
-else $method="index";
-
-app()->run(\DeafCity\Controllers\Contact::class, $method); 
+header('Location: /', true, 302);
+exit;
--- index.php
+++ index.php
@@ -1,5 +1,9 @@
-<?php
+<!DOCTYPE html>
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<body>
 
-require("src/autoload.php");
+<h1 style="font-size:10vw; color: #007800">DEAF.city will be back soon! DEAF.city tornarà aviat! DEAF.city tornerà presto! DEAF.city regresará pronto! !سيعود DEAF.city قريبًا DEAF.city sera bientôt de retour! DEAF.city laster itzuliko da! DEAF.city voltará em breve! <a href="https://www.instagram.com/antoni__abad?igsh=dHNzZDZzYnc5a2Ft" target="_blank">@antoni__abad</a></h1>
 
-app()->run();
+</body>
+</html>
